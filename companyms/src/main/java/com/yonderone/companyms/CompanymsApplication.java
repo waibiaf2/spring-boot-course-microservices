02c58750a9ec8ec57a6d1d621183ac8a3ae60b5c
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-06 15:59:42 +0300

    Add company microservice

---
+++ companyms/src/main/java/com/yonderone/companyms/CompanymsApplication.java
@@ -0,0 +1,11 @@
+package com.yonderone.companyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanymsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CompanymsApplication.class, args);
+	}
+}
